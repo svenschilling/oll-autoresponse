3f7c34c16c47fdbab2c83f4f0917378bdd8833bf
svenschilling <sven.schilluing@gmail.com>
2024-12-07 00:48:44 +0100

    first upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from ollama import chat
+from ollama import ChatResponse
+
+
+def main():
+    response: ChatResponse = chat(model='qwen2.5-coder:32b', messages=[
+        {
+            'role': 'user', 
+            'content': 'is the sky blue?'
+        }
+    ])
+
+    print(response.message.content)
+
+
+if __name__ == '__main__':
+    main()
